18ae7380fe8820259c57b09a5863babcd0b1f4d0
valentin-vinel <valentinvnl84@gmail.com>
2026-03-09 11:40:39 +0100

    feat: CRUD project controller

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param, Body, ParseIntPipe } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
 
 @Controller('projects')
 export class ProjectsController {
-    constructor (private readonly projectsService: ProjectsService) {}
+  constructor(private readonly projectsService: ProjectsService) {}
 
-    @Get()
-    findAll() {
-        return this.projectsService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.projectsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.projectsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreateProjectDto) {
+    return this.projectsService.create(dto);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateProjectDto) {
+    return this.projectsService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.projectsService.remove(id);
+  }
 }
